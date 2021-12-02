01c1a8a33d70c1995bd118f12a285f734cc72a4b
cfbonner <cfbonner@gmail.com>
2021-12-02 21:19:48 +0000

    Day one problem solution

--- apps/sonar_sweep/lib/sonar_sweep.ex
+++ apps/sonar_sweep/lib/sonar_sweep.ex
@@ -4,15 +4,31 @@ defmodule SonarSweep do
   """
 
   @doc """
-  Hello world.
+
+  Calculate the depth increase from dataset
 
   ## Examples
 
-      iex> SonarSweep.hello()
-      :world
+      # Problem one: Comparing adjacent values
+
+      iex> SonarSweep.calculate_depth_increase('lib/depth_report.txt')
+      1766
+
+      # Problem two: Comparing batches
+
+      iex> SonarSweep.calculate_depth_increase('lib/depth_report.txt', 3)
+      1797
 
   """
-  def hello do
-    :world
+  def calculate_depth_increase(report, breadth \\ 1) do
+    {:ok, file} = File.read(report)
+
+    file
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(breadth, 1, :discard)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.filter(fn [first, second] -> Enum.sum(second) > Enum.sum(first) end)
+    |> Enum.count()
   end
 end
